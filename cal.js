800cad0159032c6c7c0aeb917c05917506b003be
Kavishka Dilshan <120158496+kavishka858@users.noreply.github.com>
2023-07-30 20:27:34 +0530

    Cofee Vavuniya

---
+++ cal.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+       var num1 = document.getElementById("firstNumber").value;
+       var num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
